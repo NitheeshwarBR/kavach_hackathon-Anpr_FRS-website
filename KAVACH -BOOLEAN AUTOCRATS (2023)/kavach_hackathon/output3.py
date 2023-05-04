d31623775ea05a39fa396db3a5446a86d4adaec9
NITHEESHWAR B R <nitheeshwar.r.g@gmail.com>
2023-05-04 18:29:45 +0530

    dummy commits

--- KAVACH -BOOLEAN AUTOCRATS (2023)/kavach_hackathon/output3.py
+++
@@ -1,9 +0,0 @@
-import cv2
-from matplotlib import pyplot as plt
-import numpy as np
-import imutils
-import easyocr
-
-img=cv2.imread('C://Users//nithe//OneDrive//Desktop//kavach_hackathon//Images//8.jpg')
-gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-plt.imshow(cv2.cvtColor(gray,cv2.COLOR_BGR2RGB))
